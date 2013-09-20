e28c18ebefc5ef12e2d63dffd830a5290ca86824
Boris Glavic <lordpretzel@gmail.com>
2013-09-20 18:07:58 -0500

    main function for transformation provenance rewrite

---
+++ src/provenance_rewriter/transformation_rewrites/transformation_prov_main.c
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------------------
+ *
+ * transformation_prov_main.c
+ *			  
+ *		
+ *		AUTHOR: lord_pretzel
+ *
+ *		
+ *
+ *-----------------------------------------------------------------------------
+ */
+
+#include "provenance_rewriter/transformation_rewrites/transformation_prov_main.h"
+
+QueryOperator *
+rewriteTransformationProvenance (QueryOperator *op)
+{
+    return op;
+}
