eabb7e86336fc0f516d17dda9c838a1c09bdf733
Ying Ni <yni6@hawk.iit.edu>
2013-10-21 21:29:06 -0500

    Add join translation (incomplete)

--- src/analysis_and_translate/translator.c
+++ src/analysis_and_translate/translator.c
@@ -14,47 +14,169 @@
 #include "model/query_block/query_block.h"
 #include "model/query_operator/query_operator.h"
 #include "model/list/list.h"
+#include <assert.h>
 
 static QueryOperator *
-translateSetQuery(SetQuery *sq, List *parents);
+translateSetQuery(SetQuery *sq);
 
 static QueryOperator *
-translateQueryBlock(QueryBlock *qb, List *parents);
+translateQueryBlock(QueryBlock *qb);
+
+static List *
+translateFromClauseToOperatorList(List *fromClause);
+
+static inline QueryOperator *
+createTableAccessOpFromFromTableRef(FromTableRef *ftr);
+
+static QueryOperator *
+translateFromJoinExpr(FromJoinExpr *fje);
+
+static QueryOperator *
+translateFromSubquery(FromSubquery *fsq);
 
 QueryOperator *
-translateParse(Node *q) {
-	return NULL;
+translateParse(Node *q)
+{
+    return NULL;
 }
 
 static QueryOperator *
-translateSetQuery(SetQuery *sq, List *parents) {
-	List *inputs = appendToTailOfList(inputs, NULL);
-	inputs = appendToTailOfList(inputs, NULL);
-	// set children of the set operator node
+translateSetQuery(SetQuery *sq)
+{
+    QueryOperator *left = NULL;
+    QueryOperator *right = NULL;
+    if (sq->lChild)
+    {
+        if (sq->lChild->type == T_SetQuery)
+            left = translateSetQuery((SetQuery *) sq->lChild);
+        else if (sq->lChild->type == T_QueryBlock)
+            left = translateQueryBlock(((QueryBlock *) sq->lChild));
+    }
+    if (sq->rChild)
+    {
+        if (sq->rChild->type == T_SetQuery)
+            right = translateSetQuery((SetQuery *) sq->rChild);
+        else if (sq->rChild->type == T_QueryBlock)
+            right = translateQueryBlock(((QueryBlock *) sq->rChild));
+    }
+    assert(left && right);
+    List *inputs = appendToTailOfList(inputs, left);
+    inputs = appendToTailOfList(inputs, right);
+    // set children of the set operator node
 
-	List *attrNames = NULL;
-	FOREACH(char, attr, sq->selectClause)
-		attrNames = appendToTailOfList(attrNames, attr);
-	// set result table's attribute names of the set operator node
+    List *attrNames = NULL;
+    if (sq->selectClause)
+    {
+        FOREACH(char, attr, sq->selectClause)
+            attrNames = appendToTailOfList(attrNames, attr);
+    } // set result table's attribute names of the set operator node
 
-	SetOperator *so = createSetOperator(sq->setOp, inputs, parents, attrNames);
-	List *pa = singleton(so);
-	// create set operator node
+    SetOperator *so = createSetOperator(sq->setOp, inputs, NULL, attrNames);
+    // create set operator node
 
-	if (sq->lChild->type == T_SetQuery)
-		_OP_LCHILD(so) = translateSetQuery((SetQuery *) sq->lChild, pa);
-	else if (sq->lChild->type == T_QueryBlock)
-		_OP_LCHILD(so) = translateQueryBlock(((QueryBlock *) sq->lChild), pa);
+    //TODO free attrNames list
 
-	if (sq->rChild->type == T_SetQuery)
-		_OP_RCHILD(so) = translateSetQuery((SetQuery *) sq->rChild, pa);
-	else if (sq->rChild->type == T_QueryBlock)
-		_OP_RCHILD(so) = translateQueryBlock(((QueryBlock *) sq->rChild), pa);
+    OP_LCHILD(so)->parents = OP_RCHILD(so)->parents = singleton(so);
+    // set the parent of the node's children
 
-	return ((QueryOperator *) so);
+    return ((QueryOperator *) so);
 }
 
 static QueryOperator *
-translateQueryBlock(QueryBlock *qb, List *parents) {
-	return NULL; //TODO
+translateQueryBlock(QueryBlock *qb)
+{
+    return NULL; //TODO
+}
+
+static List *
+translateFromClauseToOperatorList(List *fromClause)
+{
+    List *opList = NULL;
+
+    FOREACH(FromItem, from, fromClause)
+    {
+        QueryOperator *op = NULL;
+        switch (from->type)
+        {
+            case T_FromTableRef:
+                op = createTableAccessOpFromFromTableRef((FromTableRef *) from);
+                break;
+            case T_FromJoinExpr:
+                op = translateFromJoinExpr((FromJoinExpr *) from);
+                break;
+            case T_FromSubquery:
+                op = translateFromSubquery((FromSubquery *) from);
+                break;
+        }
+
+        assert(op);
+        opList = appendToTailOfList(opList, op);
+    }
+
+    assert(opList);
+    return opList;
+}
+
+static inline QueryOperator *
+createTableAccessOpFromFromTableRef(FromTableRef *ftr)
+{
+    TableAccessOperator *ta = createTableAccessOp(ftr->tableId, ftr->from.name,
+    NULL, ftr->from.attrNames, NULL); // TODO  get data types
+    return ((QueryOperator *) ta);
+}
+
+static QueryOperator *
+translateFromJoinExpr(FromJoinExpr *fje)
+{
+    QueryOperator *input1 = NULL;
+    QueryOperator *input2 = NULL;
+    switch (fje->left->type)
+    {
+        case T_FromTableRef:
+            input1 = createTableAccessOpFromFromTableRef(
+                    (FromTableRef *) fje->left);
+            break;
+        case T_FromJoinExpr:
+            input1 = translateFromJoinExpr((FromJoinExpr *) fje->left);
+            break;
+        case T_FromSubquery:
+            input1 = translateFromSubquery((FromSubquery *) fje->left);
+            break;
+    }
+    switch (fje->right->type)
+    {
+        case T_FromTableRef:
+            input2 = createTableAccessOpFromFromTableRef(
+                    (FromTableRef *) fje->right);
+            break;
+        case T_FromJoinExpr:
+            input2 = translateFromJoinExpr((FromJoinExpr *) fje->right);
+            break;
+        case T_FromSubquery:
+            input2 = translateFromSubquery((FromSubquery *) fje->right);
+            break;
+    }
+
+    assert(input1 && input2);
+    List *inputs = appendToTailOfList(inputs, input1);
+    inputs = appendToTailOfList(inputs, input2);
+    // set children of the join operator node
+
+    JoinOperator *jo = createJoinOp(fje->joinType, fje->cond, inputs, NULL,
+            fje->from.attrNames); //TODO copy cond? compute attrNames?
+    // create join operator node
+
+    OP_LCHILD(jo)->parents = OP_RCHILD(jo)->parents = singleton(jo);
+    // set the parent of the node's children
+
+    return ((QueryOperator *) jo);
+}
+
+static QueryOperator *
+translateFromSubquery(FromSubquery *fsq)
+{
+    if (fsq->subquery->type == T_SetQuery)
+        return translateSetQuery((SetQuery *) fsq->subquery);
+    else
+        return translateQueryBlock((QueryBlock *) fsq->subquery);
 }
