782117caa2ae9c9d4ba07c3badf636f2be484b30
Boris Glavic <lordpretzel@gmail.com>
2013-10-25 09:08:08 -0500

    added translate update skeleton

---
+++ src/analysis_and_translate/translate_update.c
@@ -0,0 +1,20 @@
+/*-----------------------------------------------------------------------------
+ *
+ * translate_update.c
+ *			  
+ *		
+ *		AUTHOR: lord_pretzel
+ *
+ *		
+ *
+ *-----------------------------------------------------------------------------
+ */
+
+#include "analysis_and_translate/translate_update.h"
+
+
+QueryOperator *
+translateUpdate (Node *update)
+{
+
+}
