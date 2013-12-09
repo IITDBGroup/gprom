05a7b96e903a2894bd9a0d3b168250dc71131eec
haoguo <hguo17@hawk.iit.edu>
2013-12-09 16:31:23 -0600

    add update to translate_update.c

--- src/analysis_and_translate/translate_update.c
+++ src/analysis_and_translate/translate_update.c
@@ -11,14 +11,75 @@
  */
 
 #include "analysis_and_translate/translate_update.h"
-
+#include "parser/parser.h"
+#include "model/query_operator/query_operator.h"
+#include "analysis_and_translate/translator.h"
 
 QueryOperator *
 translateUpdate (Node *update)
 {
-    Node *q;
+    //for example UPDATE R SET a = a+2 WHERE C>5
+    StringInfo update = makeStringInfo();
+    Node *update;
 
-    // test
+    if(!cond)
+   {
+    TableAccessOperator *ta = makeNode(TableAccessOperator);
+    ta->tableName = tableName;
 
-    return NULL;
+    SelectionOperator *sel = makeNode(SelectionOperator);
+    sel->cond = copyObject(cond);
+
+    ProjectionOperator *prj = makeNode(ProjectionOperator);
+    prj->projExprs = appendToTailOfList(prj->projExprs, (Node *) copyObject(expr));
+    
+    SetOperator *set = makeNode(SetOperator);
+    set->setOpType = setOpType;
+   }   
+   return update;
 }
+
+
+QueryOperator *
+translateInsert (Node *insert)
+{
+    StringInfo insert = makeStringInfo();
+    Node *insert;
+
+    if(!cond)
+   {
+    TableAccessOperator *ta = makeNode(TableAccessOperator);
+    ta->tableName = tableName;
+
+    SelectionOperator *sel = makeNode(SelectionOperator);
+    sel->cond = copyObject(cond);
+
+    ProjectionOperator *prj = makeNode(ProjectionOperator);
+    prj->projExprs = appendToTailOfList(prj->projExprs, (Node *) copyObject(expr));
+    
+    SetOperator *set = makeNode(SetOperator);
+    set->setOpType = setOpType;
+   }   
+   return insert; 
+}
+
+QueryOperator *
+translateDelete (Node *delete)
+{
+    StringInfo delete = makeStringInfo();
+    Node *delete;
+
+    if(!cond)
+   {
+    TableAccessOperator *ta = makeNode(TableAccessOperator);
+    ta->tableName = tableName;
+   }   
+   return delete;   
+}
+
+
+
+
+
+
+
