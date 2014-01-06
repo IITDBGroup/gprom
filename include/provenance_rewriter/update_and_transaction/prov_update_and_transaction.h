d89745a0ea9702d8834db2a0e64c9dc305ebe24d
Boris Glavic <lordpretzel@gmail.com>
2014-01-06 17:10:16 -0600

    added update_and_transaction provenance preprocessing: merging sequences of updates that have been translated into queries into a single query before applpying provenance rewrite rules

---
+++ include/provenance_rewriter/update_and_transaction/prov_update_and_transaction.h
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------------------
+ *
+ * prov_update_and_transaction.h
+ *		
+ *
+ *		AUTHOR: lord_pretzel
+ *
+ *-----------------------------------------------------------------------------
+ */
+
+#ifndef PROV_UPDATE_AND_TRANSACTION_H_
+#define PROV_UPDATE_AND_TRANSACTION_H_
+
+#include "model/query_operator/query_operator.h"
+
+void mergeUpdateSequence(ProvenanceComputation *op);
+
+
+#endif /* PROV_UPDATE_AND_TRANSACTION_H_ */
