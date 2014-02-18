80b5f2a45d5efc136d1cb428c2df0267bcf52725
zephyr <zlin19@hawk.iit.edu>
2014-02-17 23:19:29 -0600

    modify testrewriter.sh

--- test/testrewriter.sh
+++ test/testrewriter.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./test/testrewriter -host fourier.cs.iit.edu -db orcl -port 1521 -user tprov -passwd "XA<w67onz" -sql "PROVENANCE AS OF SCN 123 OF(SELECT sum(b) FROM r GROUP BY a;);" -loglevel 3
+./test/testrewriter -host fourier.cs.iit.edu -db orcl -port 1521 -user tprov -passwd "XA<w67onz" -sql "PROVENANCE AS OF SCN 123 OF(SELECT sum(b) FROM r GROUP BY a,b;);" -loglevel 3
