a8ad95d80759017f52121272acf60425869529a9
Boris Glavic <lordpretzel@gmail.com>
2014-02-24 17:33:05 -0600

    added script for running debugger on testrewriter.c

---
+++ test/debugrewriter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$#X" != "2X" ]; then
+    echo "give two parameters, the first one is loglevel, the second one is SQL code"
+    echo "testrewriters.sh 3 \"SELECT * FROM r;\""
+    exit 1
+fi
+LOGLEVEL=$1
+SQL=$2
+SCRIPT=debug.script
+
+echo "run -host ligeti.cs.iit.edu -db orcl -port 1521 -user fga_user -passwd "fga" -log -loglevel ${LOGLEVEL} -sql \"${SQL}\"" > ./$SCRIPT
+gdb test/testrewriter -x $SCRIPT
+#rm -f $SCRIPT
