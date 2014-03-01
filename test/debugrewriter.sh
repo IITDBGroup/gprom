49ca198b7be1cab20882a595540e354bcf8608c6
Boris Glavic <lordpretzel@gmail.com>
2014-02-28 22:01:04 -0600

    scripts updated

--- test/debugrewriter.sh
+++ test/debugrewriter.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-if [ "$#X" != "2X" ]; then
-    echo "give two parameters, the first one is loglevel, the second one is SQL code"
-    echo "testrewriters.sh 3 \"SELECT * FROM r;\""
+if [ $# -le 2 ]; then
+    echo "give at least two parameters, the first one is loglevel, the second one is SQL code"
+    echo "debugrewriters.sh 3 \"SELECT * FROM r;\""
     exit 1
 fi
 LOGLEVEL=$1
 SQL=$2
+ARGS="${*:3}"
 SCRIPT=debug.script
 
-echo "run -host ligeti.cs.iit.edu -db orcl -port 1521 -user fga_user -passwd "fga" -log -loglevel ${LOGLEVEL} -sql \"${SQL}\"" > ./$SCRIPT
+echo "run -host ligeti.cs.iit.edu -db orcl -port 1521 -user fga_user -passwd "fga" -log -loglevel ${LOGLEVEL} -sql \"${SQL}\" ${ARGS}" > ./$SCRIPT
 gdb test/testrewriter -x $SCRIPT
 #rm -f $SCRIPT
