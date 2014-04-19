bd2676be2f11ce223f569bd063d60203d629a9c6
Boris Glavic <lordpretzel@gmail.com>
2014-04-18 21:37:42 -0500

    debug translate

---
+++ test/debugtranslate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $# -le 1 ]; then
+    echo "give at least two parameters, the first one is loglevel, the second one is SQL code"
+    echo "debugtranslate.sh 3 \"SELECT * FROM r;\""
+    exit 1
+fi
+LOGLEVEL=$1
+SQL=$2
+ARGS="${*:3}"
+SCRIPT=debug.script
+
+echo "run -host ligeti.cs.iit.edu -db orcl -port 1521 -user fga_user -passwd "fga" -log -loglevel ${LOGLEVEL} -activate treefiy_prov_rewrite_input -sql \"${SQL}\" ${ARGS}" > ./$SCRIPT
+gdb test/testrewriter -x $SCRIPT
+#rm -f $SCRIPT
