39ebb3323675bc2eb70fbdf272b2d7cc7ba8032b
Boris Glavic <lordpretzel@gmail.com>
2014-10-21 22:11:10 -0500

    fixed merge problem with sql_serializer

--- src/sql_serializer/sql_serializer.c
+++ src/sql_serializer/sql_serializer.c
@@ -44,102 +44,10 @@ serializeOperatorModel(Node *q)
 char *
 serializeQuery(QueryOperator *q)
 {
-    StringInfo str;
-    StringInfo viewDef;
-    char *result;
-
-    NEW_AND_ACQUIRE_MEMCONTEXT("SQL_SERIALIZER");
-
-    str = makeStringInfo();
-    viewDef = makeStringInfo();
-
-    // initialize basic structures and then call the worker
-    viewMap = NULL;
-    viewNameCounter = 0;
-
-    // simulate non Oracle conformant data types and expressions (boolean)
-    makeDTOracleConformant(q);
-
-    // call main entry point for translation
-    serializeQueryOperator (q, str);
-
-    /*
-     *  prepend the temporary view definition to create something like
-     *      WITH a AS (q1), b AS (q2) ... SELECT ...
-     */
-    if (HASH_COUNT(viewMap) > 0)
-    {
-        appendStringInfoString(viewDef, "WITH ");
-
-        // loop through temporary views we have defined
-        for(TemporaryViewMap *view = viewMap; view != NULL; view = view->hh.next)
-        {
-            appendStringInfoString(viewDef, view->viewDefinition);
-            if (view->hh.next != NULL)
-                appendStringInfoString(viewDef, ",\n\n");
-        }
-
-        // prepend to query translation
-        DEBUG_LOG("views are:\n\n%s", viewDef->data);
-        result = CONCAT_STRINGS(viewDef->data, str->data);
-    }
-    else
-        result = str->data;
-
-    // copy result to callers memory context and clean up
-    FREE_MEM_CONTEXT_AND_RETURN_STRING_COPY(result);
+    ASSERT(plugin);
+    return plugin->serializeQuery(q);
 }
 
-/*
- * Replace boolean data types
- */
-static void
-makeDTOracleConformant(QueryOperator *q)
-{
-    replaceNonOracleDTs((Node *) q, NULL);
-}
-
-static boolean
-replaceNonOracleDTs (Node *node, void *context)
-{
-    if (node == NULL)
-        return TRUE;
-
-    //TODO keep context
-    //TODO take care of boolean expressions that are used where Oracle expects
-    if (isA(node,Constant))
-    {
-        Constant *c = (Constant *) node;
-
-        if (c->constType == DT_BOOL)
-        {
-            c->constType = DT_INT;
-            if (!c->isNull)
-            {
-                boolean val = BOOL_VALUE(c);
-                c->value = NEW(int);
-                INT_VALUE(c) = val ? 1 : 0;
-            }
-        }
-    }
-
-    return visit(node, replaceNonOracleDTs, context);
-}
-
-/*
- * Main entry point for serialization.
- */
-static List *
-serializeQueryOperator (QueryOperator *q, StringInfo str)
-{
-    // operator with multiple parents
-    if (LIST_LENGTH(q->parents) > 1 || HAS_STRING_PROP(q,PROP_MATERIALIZE))
-        return createTempView (q, str);
-    else if (isA(q, SetOperator))
-        return serializeSetOperator(q, str);
-    else
-        return serializeQueryBlock(q, str);
-}
 
 char *
 quoteIdentifier (char *ident)
