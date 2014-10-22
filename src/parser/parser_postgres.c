1bf89465e8d81a3e15c2810b33f48ee493378c99
Boris Glavic <lordpretzel@gmail.com>
2014-10-21 20:58:44 -0500

    add yacc and lex headers to BUILD_SOURCE to force automake to build them first

--- src/parser/parser_postgres.c
+++ src/parser/parser_postgres.c
@@ -15,9 +15,8 @@
 #include "mem_manager/mem_mgr.h"
 #include "parser/parser.h"
 #include "parser/parse_internal_postgres.h"
-#include "parser/postgres_parser.tab.h"
+#include "postgres_parser.tab.h"
 #include "parser/parser_postgres.h"
-#include "sql_parser.tab.h"
 #include "instrumentation/timing_instrumentation.h"
 
 static Node *parseInternalPostgres (void);
