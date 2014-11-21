296948839bc81181bbcb7c97dca060d41451ba35
Boris Glavic <lordpretzel@gmail.com>
2014-11-20 18:18:30 -0600

    convenience scripts

---
+++ GPsql.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PROGRAM="${2}"
+LOG="${1}"
+./test/testrewriter.sh ${LOG} "${PROGRAM}" -Pparser dl -Panalyzer dl -Ptranslator dl -Pexecutor sql -Cattr_reference_consistency FALSE -Cunique_attr_names FALSE -Cschema_consistency FALSE -activate optimize_operator_model
