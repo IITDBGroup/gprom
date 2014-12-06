fc3f476557fbf42291d68a4358ed066c8dfb3d79
Boris Glavic <lordpretzel@gmail.com>
2014-12-06 09:43:22 -0600

    genGP

--- genGp.sh
+++ genGp.sh
@@ -2,7 +2,7 @@
 PROGRAM="${1}"
 OUT="${2}"
 echo "-- gen prov"
-./test/testrewriter.sh 0 "${1}" -Pparser dl -Panalyzer dl -Ptranslator dl -Pexecutor gp -Cattr_reference_consistency FALSE -Cunique_attr_names FALSE -Cschema_consistency FALSE -activate optimize_operator_model > ${OUT}.dot
+./test/testrewriter.sh 0 "${1}" -Pparser dl -Panalyzer dl -Ptranslator dl -Pexecutor gp -Cattr_reference_consistency FALSE -Cunique_attr_names FALSE -Cschema_consistency FALSE -activate optimize_operator_model  -Oremove_redundant_projections FALSE  > ${OUT}.dot
 echo "-- run graphviz on ${OUT}.dot"
 dot -Tpdf -o ${OUT}.pdf ${OUT}.dot
 open ${OUT}.pdf
