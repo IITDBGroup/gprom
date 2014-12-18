59f0fef9be7191e811515ed3a02c6878d8c327a4
Boris Glavic <lordpretzel@gmail.com>
2014-12-18 16:01:44 -0600

    optimizer prop files

---
+++ src/operator_optimizer/optimizer_prop_inference.c
@@ -0,0 +1,31 @@
+/*-----------------------------------------------------------------------------
+ *
+ * optimizer_prop_inference.c
+ *			  
+ *		
+ *		AUTHOR: lord_pretzel
+ *
+ *		
+ *
+ *-----------------------------------------------------------------------------
+ */
+
+#include "operator_optimizer/optimizer_prop_inference.h"
+
+void
+computeKeyProp (QueryOperator *root)
+{
+
+}
+
+void
+computeECProp (QueryOperator *root)
+{
+
+}
+
+void
+computeSetProp (QueryOperator *root)
+{
+
+}
