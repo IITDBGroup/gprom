8bc5c555a5147675c9aa818654fa837ae94e1605
Saba_d <sdasadaw@hawk.iit.edu>
2015-07-01 16:10:32 -0500

    select+prov working

--- javatest/org/gprom/jdbc/test/Test.java
+++ javatest/org/gprom/jdbc/test/Test.java
@@ -19,44 +19,47 @@ import com.sun.jna.Native;
 
 /**
  * @author lord_pretzel
- *
+ * 
  */
 public class Test {
 
 	static Logger log = Logger.getLogger(Test.class);
-	
-	public static void main (String[] args) throws Exception {
+
+	public static void main(String[] args) throws Exception {
 		PropertyConfigurator.configureAndWatch("javalib/log4j.properties");
 		String driverURL = "oracle.jdbc.OracleDriver";
-		//String url = "jdbc:hsqldb:file:/Users/alex/db/mydb";
+		// String url = "jdbc:hsqldb:file:/Users/alex/db/mydb";
 		String username = "fga_user";
 		String password = "fga";
 		String host = "ligeti.cs.iit.edu";
 		String port = "1521";
 		String sid = "orcl";
-		String url = "jdbc:gprom:oracle:thin:" + username + "/" + password + 
-				"@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=" + host + ")(PORT=" + port + ")))(CONNECT_DATA=(SID=" + sid +")))";
+		String url = "jdbc:gprom:oracle:thin:" + username + "/" + password
+				+ "@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST="
+				+ host + ")(PORT=" + port + ")))(CONNECT_DATA=(SID=" + sid
+				+ ")))";
 		GProMConnection con = null;
 		Native.setProtected(true);
 		log.error("Proetcted: " + Native.isProtected());
-		try{
+		try {
 			Class.forName("org.gprom.jdbc.driver.GProMDriver");
 			Class.forName(driverURL);
-//			w.setLogLevel(2);
-		} catch(ClassNotFoundException e){
+			// w.setLogLevel(2);
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 			System.err.println("Install the needed driver first");
 			System.exit(-1);
 		}
-		try{
+		try {
 			Properties info = new Properties();
 			info.setProperty(GProMDriverProperties.JDBC_METADATA_LOOKUP, "TRUE");
 			info.setProperty("user", username);
 			info.setProperty("password", password);
-			con = (GProMConnection) DriverManager.getConnection(url,info);
-		} catch (SQLException e){
+			con = (GProMConnection) DriverManager.getConnection(url, info);
+		} catch (SQLException e) {
 			e.printStackTrace();
-			System.err.println("Something went wrong while connecting to the database.");
+			System.err
+					.println("Something went wrong while connecting to the database.");
 			System.exit(-1);
 		}
 		System.out.println("Connection was successfully");
@@ -66,65 +69,94 @@ public class Test {
 		con.getW().setBoolOption("optimize_operator_model", true);
 		con.getW().setBoolOption("aggressive_model_checking", true);
 		log.error("log.level=" + con.getW().getIntOption("log.level"));
-		log.error("log.active=" +  con.getW().getBoolOption("log.active"));
-		log.error("pi_cs_use_composable=" +  con.getW().getBoolOption("pi_cs_use_composable"));
-		log.error("optimize_operator_model=" +  con.getW().getBoolOption("optimize_operator_model"));
+		log.error("log.active=" + con.getW().getBoolOption("log.active"));
+		log.error("pi_cs_use_composable="
+				+ con.getW().getBoolOption("pi_cs_use_composable"));
+		log.error("optimize_operator_model="
+				+ con.getW().getBoolOption("optimize_operator_model"));
 		log.error("type of log.level: " + con.getW().typeOfOption("log.level"));
-		log.error("type of log.activate: " + con.getW().typeOfOption("log.active"));
-		log.error("type of connection.port: " + con.getW().typeOfOption("connection.port"));
-		log.error("type of connection.user: " + con.getW().typeOfOption("connection.user"));
-		log.error("type of connection.passwd: " + con.getW().typeOfOption("connection.passwd"));
-		log.error("type of connection.host: " + con.getW().typeOfOption("connection.host"));
-		log.error("type of connection.db: " + con.getW().typeOfOption("connection.db"));
-		
+		log.error("type of log.activate: "
+				+ con.getW().typeOfOption("log.active"));
+		log.error("type of connection.port: "
+				+ con.getW().typeOfOption("connection.port"));
+		log.error("type of connection.user: "
+				+ con.getW().typeOfOption("connection.user"));
+		log.error("type of connection.passwd: "
+				+ con.getW().typeOfOption("connection.passwd"));
+		log.error("type of connection.host: "
+				+ con.getW().typeOfOption("connection.host"));
+		log.error("type of connection.db: "
+				+ con.getW().typeOfOption("connection.db"));
+
 		Statement st = con.createStatement();
-		
-		
+
 		log.error("statement created");
-		
+
 		Scanner in = new Scanner(System.in);
-		 
-	      System.out.println("Enter a string");
-	      String s = in.nextLine();
-		
+
+		System.out.println("Enter a string");
+		String s = in.nextLine();
 		ResultSet rs = st.executeQuery(s);
 		printResult(rs);
-		String x = s.replace(";", "");
-		rs = st.executeQuery("PROVENANCE OF ("+x+");");
-		printResult(rs);
-		
-		// test error
-		try {
-		rs = st.executeQuery("PROVENANCE OF ("+x+");");
+		if (!s.toLowerCase().contains("provenance")) {
+
+			String x = s.replace(";", "");
+			rs = st.executeQuery("PROVENANCE OF (" + x + ");");
+			printResult(rs);
+
+			// test error
+			try {
+				rs = st.executeQuery("PROVENANCE OF (" + x + ");");
+			} catch (Exception e) {
+				System.out.printf("%s", e);
+			}
+			/*
+			 * rs = st.executeQuery("PROVENANCE OF ("+x+");"); printResult(rs);
+			 */
+
+			log.error("statement shutdown");
+			con.close();
+		} else {
+
+			rs = st.executeQuery(s);
+			printResult(rs);
+
+			// test error
+			try {
+				rs = st.executeQuery(s);
+			} catch (Exception e) {
+				System.out.printf("%s", e);
+			}
+
+			/*
+			 * rs = st.executeQuery(s); printResult(rs);
+			 */
+
+			log.error("statement shutdown");
+			con.close();
 		}
-		catch (Exception e) {
-			System.out.printf("%s", e);
-		}
-		
-		rs = st.executeQuery("PROVENANCE OF ("+x+");");
-		printResult(rs);
-		
-		log.error("statement shutdown");
-		con.close();
 	}
 
 	private static void printResult(ResultSet rs) throws SQLException {
 		/********************************************************************************/
 		System.out.println();
-		System.out.println("-------------------------------------------------------------------------------");
-		for(int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
+		System.out
+				.println("-------------------------------------------------------------------------------");
+		for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
 			System.out.print(rs.getMetaData().getColumnLabel(i) + "\t|");
 		System.out.println();
-		System.out.println("-------------------------------------------------------------------------------");
-		
-		while(rs.next()) {		
-			for(int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
+		System.out
+				.println("-------------------------------------------------------------------------------");
+
+		while (rs.next()) {
+			for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
 				System.out.print(rs.getString(i) + "\t|");
 			System.out.println();
 		}
-		System.out.println("-------------------------------------------------------------------------------");
+		System.out
+				.println("-------------------------------------------------------------------------------");
 		System.out.println();
 		System.out.println();
 	}
-	
+
 }
