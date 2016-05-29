06719ce9d0c95e56f98a3348d5ac61ca234593a8
Boris Glavic <lordpretzel@gmail.com>
2016-05-29 09:35:40 -0500

    new memory context implementation

--- src/mem_manager/mem_mgr.c
+++ src/mem_manager/mem_mgr.c
@@ -39,13 +39,16 @@ typedef struct MemContextNode
     struct MemContextNode *next;
 } MemContextNode; // context stack node
 
-static inline void addAlloc(MemContext *mc, void *addr, const char *file,
-        unsigned line);
-static inline void delAlloc(MemContext *mc, void *addr, const char *file,
-        unsigned line);
-static inline void freeAlloc (Allocation *alloc, MemContext *mc, void *addr,
-        const char *file, unsigned line);
+static inline void createFirstChunk(MemContext *mc);
 
+//static inline void addAlloc(MemContext *mc, void *addr, const char *file,
+//        unsigned line);
+//static inline void delAlloc(MemContext *mc, void *addr, const char *file,
+//        unsigned line);
+//static inline void freeAlloc (Allocation *alloc, MemContext *mc, void *addr,
+//        const char *file, unsigned line);
+static inline void createChunk (MemContext *mc, size_t size, const char *file,
+        unsigned line);
 
 static MemContext *curMemContext = NULL; // global pointer to current memory context
 static MemContext *defaultMemContext = NULL;
@@ -155,84 +158,84 @@ releaseCurMemContext(const char *file, unsigned line)
 /*
  * Adds allocated memory information to a memory context.
  */
-static void
-addAlloc(MemContext *mc, void *addr, const char *file, unsigned line)
-{
-    Allocation *newAlloc = (Allocation *) malloc(sizeof(Allocation));
-    ASSERT(mc != NULL);
-    newAlloc->address = addr;
-    newAlloc->file = file;
-    newAlloc->line = line;
-    HASH_ADD_PTR(mc->hashAlloc, address, newAlloc); // add to hash table. Use address as key
-
-    GENERIC_LOG(LOG_TRACE, file, line,
-        "Added [addr:%p, file:'%s', line:%u] to memory context '%s'.", addr,
-        file, line, mc->contextName);
-}
+//static void
+//addAlloc(MemContext *mc, void *addr, const char *file, unsigned line)
+//{
+//    Allocation *newAlloc = (Allocation *) malloc(sizeof(Allocation));
+//    ASSERT(mc != NULL);
+//    newAlloc->address = addr;
+//    newAlloc->file = file;
+//    newAlloc->line = line;
+//    HASH_ADD_PTR(mc->hashAlloc, address, newAlloc); // add to hash table. Use address as key
+//
+//    GENERIC_LOG(LOG_TRACE, file, line,
+//        "Added [addr:%p, file:'%s', line:%u] to memory context '%s'.", addr,
+//        file, line, mc->contextName);
+//}
 
 /*
  * Removes memory allocation information from a memory context.
  */
-static void
-delAlloc(MemContext *mc, void *addr, const char *file, unsigned line)
-{
-    Allocation *alloc = NULL;
-    HASH_FIND_PTR(mc->hashAlloc, &addr, alloc); // find allocation info by address first
-    if (alloc)
-    {
-        freeAlloc(alloc, mc, addr, file, line);
-        return;
-    }
-    // allocation not found search in ancestor contexts on the stack
-    else
-    {
-        MemContextNode *c = topContextNode;
-        MemContext *curC;
-
-        GENERIC_LOG(LOG_TRACE, file, line,
-                "Not found [addr:%p] from memory context '%s'.",
-                            addr, mc->contextName);
-
-        while(c->mc != mc)
-            c = c->next;
-        c = c->next;
-
-        for(; c != NULL; c = c->next)
-        {
-            curC = c->mc;
-            HASH_FIND_PTR(curC->hashAlloc, &addr, alloc);
-
-            if (alloc)
-            {
-                freeAlloc(alloc, curC, addr, file, line);
-                return;
-            }
-            else
-                GENERIC_LOG(LOG_TRACE, file, line,
-                        "Not found [addr:%p] from memory context '%s'.",
-                        addr, curC->contextName);
-       }
-    }
-
-    // did not find allocation
-    FATAL_LOG("did not find allocation info for <%p> in memory context <%s>", addr, mc->contextName);
-}
+//static void
+//delAlloc(MemContext *mc, void *addr, const char *file, unsigned line)
+//{
+//    Allocation *alloc = NULL;
+//    HASH_FIND_PTR(mc->hashAlloc, &addr, alloc); // find allocation info by address first
+//    if (alloc)
+//    {
+//        freeAlloc(alloc, mc, addr, file, line);
+//        return;
+//    }
+//    // allocation not found search in ancestor contexts on the stack
+//    else
+//    {
+//        MemContextNode *c = topContextNode;
+//        MemContext *curC;
+//
+//        GENERIC_LOG(LOG_TRACE, file, line,
+//                "Not found [addr:%p] from memory context '%s'.",
+//                            addr, mc->contextName);
+//
+//        while(c->mc != mc)
+//            c = c->next;
+//        c = c->next;
+//
+//        for(; c != NULL; c = c->next)
+//        {
+//            curC = c->mc;
+//            HASH_FIND_PTR(curC->hashAlloc, &addr, alloc);
+//
+//            if (alloc)
+//            {
+//                freeAlloc(alloc, curC, addr, file, line);
+//                return;
+//            }
+//            else
+//                GENERIC_LOG(LOG_TRACE, file, line,
+//                        "Not found [addr:%p] from memory context '%s'.",
+//                        addr, curC->contextName);
+//       }
+//    }
+//
+//    // did not find allocation
+//    FATAL_LOG("did not find allocation info for <%p> in memory context <%s>", addr, mc->contextName);
+//}
 
 /*
  * Free an allocation
  */
 
-static void
-freeAlloc (Allocation *alloc, MemContext *mc, void *addr, const char *file, unsigned line)
-{
-    const char *tmpfile = alloc->file;
-    int tmpline = alloc->line;
-    HASH_DEL(mc->hashAlloc, alloc); // remove the allocation info
-    free(alloc);
-    GENERIC_LOG(LOG_TRACE, file, line,
-            "Deleted [addr:%p, file:'%s', line:%d] from memory context '%s'.",
-            addr, tmpfile, tmpline, mc->contextName);
-}
+//static void
+//freeAlloc (Allocation *alloc, MemContext *mc, void *addr, const char *file, unsigned line)
+//{
+//    const char *tmpfile = alloc->file;
+//    int tmpline = alloc->line;
+//    HASH_DEL(mc->hashAlloc, alloc); // remove the allocation info
+////    free(alloc);
+//    GENERIC_LOG(LOG_TRACE, file, line,
+//            "Deleted [addr:%p, file:'%s', line:%d] from memory context '%s'.",
+//            addr, tmpfile, tmpline, mc->contextName);
+//}
 
 
 /*
@@ -244,9 +247,27 @@ newMemContext(char *contextName, const char *file, unsigned line)
     MemContext *mc = (MemContext *) malloc(sizeof(MemContext));
     mc->contextName = contextName;
     mc->hashAlloc = NULL;
+    mc->chunks = (char **) malloc(INIT_CHUNK_ARRAY_SIZE * sizeof(char*));
+    mc->chunkSizes = (unsigned long*) malloc(INIT_CHUNK_ARRAY_SIZE * sizeof(unsigned long));
+    mc->curChunkArraySize = INIT_CHUNK_ARRAY_SIZE;
+    mc->numChunks = 1;
+
+    /* create first chunk */
+    createFirstChunk(mc);
+
     GENERIC_LOG(LOG_DEBUG, file, line, "Created memory context '%s'.",
             mc->contextName);
     return mc;
+}
+
+static inline void
+createFirstChunk(MemContext *mc)
+{
+    mc->chunks[0] = (char *) malloc(DEFAULT_CHUNK_SIZE);
+    mc->chunkSizes[0] = DEFAULT_CHUNK_SIZE;
+    mc->curAllocPos = mc->chunks[0];
+    mc->memLeftInChunk = DEFAULT_CHUNK_SIZE;
+    mc->numChunks = 1;
 }
 
 /*
@@ -292,10 +313,18 @@ void
 clearCurMemContext(const char *file, unsigned line)
 {
     Allocation *curAlloc, *tmp;
-    HASH_ITER(hh, curMemContext->hashAlloc, curAlloc, tmp)
+    MemContext *c = curMemContext;
+    HASH_ITER(hh, c->hashAlloc, curAlloc, tmp)
     {
         free_(curAlloc->address, file, line);
     }
+    for(int i = 0; i < c->numChunks; i++)
+    {
+        free(c->chunks[i]);
+        c->chunks[i] = NULL;
+    }
+    c->numChunks = 0;
+    c->memLeftInChunk = 0;
 }
 
 /*
@@ -310,16 +339,19 @@ freeCurMemContext(const char *file, unsigned line)
         int size = memContextSize(curMemContext);
         char *name = curMemContext->contextName;
         if (size > 0)
-        {
+//        {
             clearCurMemContext(file, line);
-            free(curMemContext);
-            curMemContext = NULL;
-        }
-        else if (size == 0)
-        {
-            free(curMemContext);
-            curMemContext = NULL;
-        }
+//        }
+//        else if (size == 0)
+//        {
+//            free(curMemContext);
+//        }
+        free(curMemContext->chunks);
+        free(curMemContext->chunkSizes);
+        free(curMemContext);
+        curMemContext = NULL;
+
+
         GENERIC_LOG(LOG_DEBUG, file, line, "Freed memory context '%s'.", name);
     }
 }
@@ -372,28 +404,95 @@ freeMemContextAndChildren(char *contextName)
 }
 
 /*
- * Allocates memory and records it in the current memory context.
+ * Adds a new chunk of at least requested size to mem context
  */
-void *
-malloc_(size_t bytes, const char *file, unsigned line)
+
+static inline void
+createChunk (MemContext *mc, size_t size, const char *file, unsigned line)
 {
-    void *mem = malloc(bytes);
-//    memset(mem, 178, bytes);
+    size_t actualSize = (size < DEFAULT_CHUNK_SIZE) ?
+            DEFAULT_CHUNK_SIZE :
+            size;
+    void *mem;
+
+    // round up to multiple of default chunk size
+    if (size % DEFAULT_CHUNK_SIZE != 0)
+    {
+        size += (DEFAULT_CHUNK_SIZE - (size % DEFAULT_CHUNK_SIZE));
+    }
+    mem = malloc(actualSize);
+
     if (mem == NULL)
     {
         GENERIC_LOG(LOG_FATAL, file, line, "Fail to malloc.");
     }
     else
     {
-        GENERIC_LOG(LOG_TRACE, file, line, "%ld bytes memory @%p allocated.", bytes,
+        GENERIC_LOG(LOG_TRACE, file, line, "%ld bytes memory @%p allocated.", size,
                 mem);
     }
+
+    // double chunk array size
+    if (mc->numChunks == mc->curChunkArraySize)
+    {
+        char **oldChunks = mc->chunks;
+        unsigned long *oldChunkSizes = mc->chunkSizes;
+        unsigned int newSize;
+        unsigned int oldSize = mc->curChunkArraySize;
+
+        mc->curChunkArraySize *= 2;
+        newSize = mc->curChunkArraySize;
+        mc->chunks = malloc(sizeof(char *) * newSize);
+        mc->chunkSizes = malloc(sizeof(unsigned long) * newSize);
+
+        // copy old arrays
+        for(int i = 0; i < oldSize; i++)
+        {
+            mc->chunks[i] = oldChunks[i];
+            mc->chunkSizes[i] = oldChunkSizes[i];
+        }
+    }
+
+    unsigned int numChunks = mc->numChunks;
+    mc->chunks[numChunks] = mem;
+    mc->chunkSizes[numChunks] = actualSize;
+    mc->curAllocPos = mc->chunks[numChunks];
+    mc->numChunks++;
+    mc->memLeftInChunk = actualSize;
+}
+
+/*
+ * Allocates memory and records it in the current memory context.
+ */
+void *
+malloc_(size_t bytes, const char *file, unsigned line)
+{
+    MemContext *c = curMemContext;
+
+    // if there is not enough memory in current chunk then allocate new chunk
+    if(c->memLeftInChunk < bytes)
+        createChunk(c, bytes, file, line);
+
+
+    void *mem = c->curAllocPos;
+    c->curAllocPos += bytes;
+    c->memLeftInChunk -= bytes;
+//    memset(mem, 178, bytes);
+//    if (mem == NULL)
+//    {
+//        GENERIC_LOG(LOG_FATAL, file, line, "Fail to malloc.");
+//    }
+//    else
+//    {
+//        GENERIC_LOG(LOG_TRACE, file, line, "%ld bytes memory @%p allocated.", bytes,
+//                mem);
+//    }
 
     // track mem allocations, but not of the context used for memory debugging
     if (opt_memmeasure && !streq(curMemContext->contextName,MEMDEBUG_CONTEXT_NAME))
         addContext(curMemContext->contextName, bytes, FALSE);
 
-    addAlloc(curMemContext, mem, file, line);
+//    addAlloc(curMemContext, mem, file, line);
 
     return mem;
 }
@@ -405,7 +504,20 @@ malloc_(size_t bytes, const char *file, unsigned line)
 void *
 calloc_(size_t bytes, unsigned count, const char *file, unsigned line)
 {
-    void *mem = calloc(count, bytes);
+    size_t allocSize = bytes * count;
+    MemContext *c = curMemContext;
+    void *mem; // = calloc(count, bytes);
+
+    // if there is not enough memory in current chunk then allocate new chunk
+    if(c->memLeftInChunk < allocSize)
+        createChunk(c, allocSize, file, line);
+
+    mem = c->curAllocPos;
+    memset(c->curAllocPos,0,allocSize);
+    c->curAllocPos += allocSize;
+    c->memLeftInChunk -= allocSize;
+
+
     if (mem == NULL)
     {
         GENERIC_LOG(LOG_ERROR, file, line, "Fail to calloc.");
@@ -419,9 +531,9 @@ calloc_(size_t bytes, unsigned count, const char *file, unsigned line)
 
     // track mem allocations, but not of the context used for memory debugging
     if (opt_memmeasure && !streq(curMemContext->contextName,MEMDEBUG_CONTEXT_NAME))
-        addContext(curMemContext->contextName, bytes, FALSE);
+        addContext(curMemContext->contextName, allocSize, FALSE);
 
-    addAlloc(curMemContext, mem, file, line);
+//    addAlloc(curMemContext, mem, file, line);
 
     return mem;
 }
@@ -435,8 +547,8 @@ free_(void *mem, const char *file, unsigned line)
 {
     if (mem)
     {
-        delAlloc(curMemContext, mem, file, line);
-        free(mem);
+//        delAlloc(curMemContext, mem, file, line);
+//        free(mem);
         GENERIC_LOG(LOG_TRACE, file, line, "Memory @%p freed.", mem);
     }
 }
