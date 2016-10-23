c7403ef4803a0e9c92b5828e061fa8055aad70a7
Boris Glavic <lordpretzel@gmail.com>
2016-10-23 11:37:40 -0500

    updated dev scripts

--- scripts/dev/valgrind_gprom.sh
+++ scripts/dev/valgrind_gprom.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+########################################
+# VARS
+# Directory this script resides in
+pushd $(dirname "${0}") > /dev/null
+DIR=$(pwd -L)
+popd > /dev/null
+# GProM binary
+CONF_FILE=.gprom
+GPROM=${DIR}/../../src/command_line/gprom
+GPROM_CONF=${DIR}/../../${CONF_FILE}
+########################################
+# READ USER CONFIGUATION
+source ${DIR}/../gprom_basic.sh
+########################################
 if [ $# -lt 2 ]; then
     echo "give at least two parameters, the first one is loglevel, the second one is SQL code"
-    echo "testrewriters.sh 3 \"SELECT * FROM r;\""
+	echo " "
+    echo "valgrind_gprom.sh 3 \"SELECT * FROM r;\""
     exit 1
 fi
-LOGLEVEL=$1
+LOG="-log -loglevel $1"
 SQL="$2"
 ARGS="${*:3}"
-valgrind --log-file=./valgrind.txt --tool=memcheck -v --track-origins=yes --leak-check=full --show-leak-kinds=all --malloc-fill=01 --free-fill=FF --leak-check=yes --dsymutil=yes ./test/testrewriter -host ligeti.cs.iit.edu -db orcl -port 1521 -user fga_user -passwd "fga" -log -loglevel ${LOGLEVEL} -sql "${SQL}" -activate treefiy_prov_rewrite_input ${ARGS}
+valgrind --log-file=./valgrind.txt --tool=memcheck -v --track-origins=yes --leak-check=full --show-leak-kinds=all --malloc-fill=01 --free-fill=FF --leak-check=yes --dsymutil=yes ${GPROM} ${CONNECTION_PARAMS} ${LOG} -sql "${SQL}" -treefiy_algebra_graph TRUE ${ARGS}
--- scripts/dev/valgrind_testmain.sh
+++ scripts/dev/valgrind_testmain.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+########################################
+# VARS
+# Directory this script resides in
+pushd $(dirname "${0}") > /dev/null
+DIR=$(pwd -L)
+popd > /dev/null
+# GProM binary
+CONF_FILE=.gprom
+GPROM=${DIR}/../../src/command_line/gprom
+GPROM_CONF=${DIR}/../../${CONF_FILE}
+########################################
+# READ USER CONFIGUATION
+source ${DIR}/../gprom_basic.sh
+########################################
 if [ $# -lt 1 ]; then
-    echo "give at least one parameters, the first one is loglevel"
-    echo "testrewriters.sh 3"
+    echo "give at least one parameters, a loglevel"
+	echo " "
+    echo "valgrind_testmain.sh 3"
     exit 1
 fi
-LOGLEVEL=$1
+LOG="-log -loglevel $1"
 ARGS="${*:2}"
 
 # SET PATH TO FIND libgprom
-DIR=$(pwd)/$(dirname "${0}")
-DYLD_LIBRARY_PATH="${DIR}/../src/libgprom/.libs:$DYLD_LIBRARY_PATH"
+DYLD_LIBRARY_PATH="${DIR}/../../src/libgprom/.libs:$DYLD_LIBRARY_PATH"
 DYLD_LIBRARY_PATH=`echo "$DYLD_LIBRARY_PATH" | /sw/bin/sed 's/::*$//'`
 export DYLD_LIBRARY_PATH
 echo "DYLD_LIBRARY_PATH ${DYLD_LIBRARY_PATH}"
 
-valgrind --log-file=./valgrind.txt --dsymutil=yes --tool=memcheck -v --track-origins=yes --leak-check=full --show-leak-kinds=all --malloc-fill=01 --free-fill=FF --leak-check=yes ./test/.libs/testmain -host ligeti.cs.iit.edu -db orcl -port 1521 -user fga_user -passwd "fga" -log -loglevel ${LOGLEVEL} ${ARGS}
+valgrind --log-file=./valgrind.txt --dsymutil=yes --tool=memcheck -v --track-origins=yes --leak-check=full --show-leak-kinds=all --malloc-fill=01 --free-fill=FF --leak-check=yes ./test/.libs/testmain ${CONNECTION_PARAMS} ${LOG} ${ARGS}
