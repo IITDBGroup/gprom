addbe1a9e814b074e8870b9b456c5bffc7d2fb0f
Boris Glavic <lordpretzel@gmail.com>
2016-11-19 11:17:05 -0600

    reset options after libgprom test

--- test/test_libgprom.c
+++ test/test_libgprom.c
@@ -25,7 +25,7 @@ static rc testLoopBackMetadata(void);
 static HashMap *options = NULL;
 
 static void setOpts (void);
-
+static void resetOpts (void);
 
 rc
 testLibGProM(void)
@@ -38,17 +38,39 @@ testLibGProM(void)
     RUN_TEST(testRewrite(), "test rewrite function");
     RUN_TEST(testLoopBackMetadata(), "test loop back metadata lookup");
 
+    resetOpts();
+
     return PASS;
+}
+
+#define RESET_OPT(_opt) setOption(_opt, STRING_VALUE(MAP_GET_STRING(options, _opt)))
+
+static void
+resetOpts (void)
+{
+     RESET_OPT("log.active");
+     RESET_OPT("log.level");
+     RESET_OPT("backend");
+     RESET_OPT("plugin.parser");
+     RESET_OPT("plugin.analyzer");
+     RESET_OPT("plugin.translator");
+     RESET_OPT(OPTION_CONN_USER);
+     RESET_OPT(OPTION_CONN_DB);
+     RESET_OPT(OPTION_CONN_PORT);
+     RESET_OPT(OPTION_CONN_HOST);
+     RESET_OPT(OPTION_CONN_PASSWD);
 }
 
 static void
 setOpts (void)
 {
     // copy connection options provided by user
-    gprom_setStringOption("backend", "sqlite");
-    gprom_setStringOption("plugin.parser", "oracle");
-    gprom_setStringOption("plugin.analyzer", "oracle");
-    gprom_setStringOption("plugin.translator", "oracle");
+    gprom_setOption("log.active", "true");
+    gprom_setOption("log.level", STRING_VALUE(MAP_GET_STRING(options, "log.level")));
+    gprom_setOption("backend", "sqlite");
+    gprom_setOption("plugin.parser", "oracle");
+    gprom_setOption("plugin.analyzer", "oracle");
+    gprom_setOption("plugin.translator", "oracle");
     gprom_setOption(OPTION_CONN_USER, STRING_VALUE(MAP_GET_STRING(options, OPTION_CONN_USER)));
     gprom_setOption(OPTION_CONN_DB, STRING_VALUE(MAP_GET_STRING(options, OPTION_CONN_DB)));
     gprom_setOption(OPTION_CONN_PORT, STRING_VALUE(MAP_GET_STRING(options, OPTION_CONN_PORT)));
