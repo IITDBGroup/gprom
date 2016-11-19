dae9c17e03bea9253174cc64a2e9925aff323f0d
Boris Glavic <lordpretzel@gmail.com>
2016-11-19 15:10:09 -0600

    travis

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -5,13 +5,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	brew update
 	brew outdated ant || brew upgrade ant
 	brew outdated libtool || brew upgrade libtool
-	brew outdated libsqlite3-dev || brew upgrade libsqlite3-dev
 	brew outdated bison || brew upgrade bison
 	brew outdated flex || brew upgrade flex
-	brew outdated autotools-dev || brew upgrade autotools-dev
-	brew outdated sqlite3 || brew upgrade sqlite3
-	brew outdated libreadline6 || brew upgrade libreadline6
-	brew outdated libreadline6-dev || brew upgrade libreadline6-dev
+	brew outdated sqlite || brew upgrade sqlite
+	brew outdated libreadline || brew upgrade libreadline
 # LINUX	
 else
 	sudo apt-get -qq update
