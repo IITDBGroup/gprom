efe1807416462913d1ad7db14a094818dd2cebe6
Boris Glavic <lordpretzel@gmail.com>
2016-11-24 13:49:12 -0600

    travis

--- travis/autogen.sh
+++ travis/autogen.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
+# Directory this script resides in
+pushd $(dirname "${0}") > /dev/null
+DIR=$(pwd -L)
+popd > /dev/null
+pushd ${DIR}/..
+
 # MAC OS X
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-	# Directory this script resides in
-	pushd $(dirname "${0}") > /dev/null
-	DIR=$(pwd -L)
-	popd > /dev/null
-	cd ${DIR}/..
 	glibtoolize
 	autoreconf --force --install
 # LINUX	
@@ -13,3 +15,5 @@ else
 	cd ${DIR}/..
 	make -f Makefile.maintainer
 fi
+
+popd > /dev/null
