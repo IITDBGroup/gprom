6e4b502cc6009caf88cf04e78cb323aa386c1575
Boris Glavic <lordpretzel@gmail.com>
2016-12-20 14:27:51 -0600

    test case

---
+++ include/symbolic_eval/expr_to_constraint.h
@@ -0,0 +1,18 @@
+/*-----------------------------------------------------------------------------
+ *
+ * expr_to_constraint.h
+ *		
+ *
+ *		AUTHOR: lord_pretzel
+ *
+ *-----------------------------------------------------------------------------
+ */
+
+#ifndef INCLUDE_SYMBOLIC_EVAL_EXPR_TO_CONSTRAINT_H_
+#define INCLUDE_SYMBOLIC_EVAL_EXPR_TO_CONSTRAINT_H_
+
+#include "model/node/nodetype.h"
+
+extern char *exprToEval(Node *expr);
+
+#endif /* INCLUDE_SYMBOLIC_EVAL_EXPR_TO_CONSTRAINT_H_ */
---
+++ src/symbolic_eval/expr_to_constraint.c
@@ -0,0 +1,21 @@
+/*-----------------------------------------------------------------------------
+ *
+ * expr_to_constraint.c
+ *			  
+ *		
+ *		AUTHOR: lord_pretzel
+ *
+ *		
+ *
+ *-----------------------------------------------------------------------------
+ */
+
+#include "common.h"
+#include "symbolic_eval/expr_to_constraint.h"
+
+
+char *
+exprToEval(Node *expr)
+{
+    return "";
+}
