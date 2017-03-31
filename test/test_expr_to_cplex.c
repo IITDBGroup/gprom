bfec0e60cb87b2a0d39949cde3813d1b97b5b71f
Boris Glavic <lordpretzel@gmail.com>
2017-03-31 13:05:25 -0500

    merged with upstream and added optimizations to both gneerated queries for cplex

--- test/test_expr_to_cplex.c
+++ test/test_expr_to_cplex.c
@@ -12,6 +12,7 @@
 
 
 #include "common.h"
+#include "exception/exception.h"
 #include "log/termcolor.h"
 #include "mem_manager/mem_mgr.h"
 #include "log/logger.h"
@@ -35,7 +36,6 @@
 #include "analysis_and_translate/translator_oracle.h"
 #include "provenance_rewriter/prov_rewriter.h"
 #include "operator_optimizer/operator_optimizer.h"
-#include "exception/exception.h"
 
 
 static ExceptionHandler handleCLIException (const char *message, const char *file, int line, ExceptionSeverity s);
@@ -56,58 +56,68 @@ int main(int argc, char* argv[]) {
 	else {
 	    TRY
 	    {
-		result = parseFromString(getStringOption("input.query"));
+	        result = (List *) parseFromString(getStringOption("input.query"));
 
-		//DEBUG_LOG("PARSE RESULT FROM STRING IS:\n%s", nodeToString(result));
-		//DEBUG_LOG("PARSE RESULT FROM STRING IS:\n%s", beatify(nodeToString(result)));
+	        //DEBUG_LOG("PARSE RESULT FROM STRING IS:\n%s", nodeToString(result));
+	        //DEBUG_LOG("PARSE RESULT FROM STRING IS:\n%s", beatify(nodeToString(result)));
 
-		//ERROR_LOG("Result of CPLEX IS: %d \n", checkCplex((List *) result));
+	        //ERROR_LOG("Result of CPLEX IS: %d \n", checkCplex((List *) result));
 
-		List *updates = dependAlgo(result);
+	        List *updates = dependAlgo(result);
 
-		ERROR_LOG("total Number of updates: %d \n", result->length - 1);
-		ERROR_LOG("Number of dependent statements: %d \n", updates->length - 2);
-		//DEBUG_LOG("PARSE RESULT FROM STRING IS:\n%s", nodeToString(updates));
-		//DEBUG_LOG("PARSE RESULT FROM STRING IS:\n%s", beatify(nodeToString(updates)));
+	        ERROR_LOG("total Number of updates: %d \n", result->length - 1);
+	        ERROR_LOG("Number of dependent statements: %d \n", updates->length - 2);
+	        //DEBUG_LOG("PARSE RESULT FROM STRING IS:\n%s", nodeToString(updates));
+	        //DEBUG_LOG("PARSE RESULT FROM STRING IS:\n%s", beatify(nodeToString(updates)));
 
-		Node *originalUp;
-		originalUp = popHeadOfListP(updates);
+	        Node *originalUp;
+	        originalUp = popHeadOfListP(updates);
 
-		ProvenanceStmt *provStat;
-		char *sql;
-		Node *qoModel;
-		provStat = createProvenanceStmt((Node *) updates);
-		provStat->provType = PROV_NONE;
-		provStat->inputType = PROV_INPUT_REENACT;
-		DEBUG_NODE_BEATIFY_LOG("prov:\n", provStat);
-		provStat = (ProvenanceStmt *) analyzeParseModel((Node *) provStat);
-		DEBUG_NODE_BEATIFY_LOG("analysis:\n", provStat);
-		qoModel = translateParseOracle((Node *) provStat);
-		DEBUG_NODE_BEATIFY_LOG("qo model:\n", provStat);
-		qoModel = provRewriteQBModel(qoModel);
-		DEBUG_NODE_BEATIFY_LOG("after prov rewrite:\n", provStat);
-		qoModel = optimizeOperatorModel(qoModel);
-		DEBUG_NODE_BEATIFY_LOG("after opt:\n", provStat);
-		//qoModel = translateParse((Node *) provStat);
-		sql = serializeOperatorModel(qoModel);
-		ERROR_LOG("SERIALIZED SQL Reenactment For Whatif Query:\n%s", sql);
+	        ProvenanceStmt *provStat;
+	        char *sql;
+	        Node *qoModel;
+	        provStat = createProvenanceStmt((Node *) updates);
+	        provStat->provType = PROV_NONE;
+	        provStat->inputType = PROV_INPUT_REENACT;
+	        DEBUG_NODE_BEATIFY_LOG("prov:\n", provStat);
+	        provStat = (ProvenanceStmt *) analyzeParseModel((Node *) provStat);
+	        DEBUG_NODE_BEATIFY_LOG("analysis:\n", provStat);
+	        qoModel = translateParseOracle((Node *) provStat);
+	        DEBUG_NODE_BEATIFY_LOG("qo model:\n", provStat);
+	        qoModel = provRewriteQBModel(qoModel);
+	        DEBUG_NODE_BEATIFY_LOG("after prov rewrite:\n", provStat);
+	        qoModel = optimizeOperatorModel(qoModel);
+	        DEBUG_NODE_BEATIFY_LOG("after opt:\n", provStat);
+	        //qoModel = translateParse((Node *) provStat);
+	        sql = serializeOperatorModel(qoModel);
+	        ERROR_LOG("SERIALIZED SQL Reenactment For Whatif Query:\n%s", sql);
 
-		//replace new update with original updates and create reenactment again
-		popHeadOfListP(updates);
-		updates = appendToHeadOfList(updates, originalUp);
-		provStat = createProvenanceStmt((Node *) updates);
-		provStat->provType = PROV_NONE;
-		provStat->inputType = PROV_INPUT_REENACT;
-		DEBUG_NODE_BEATIFY_LOG("prov:\n", provStat);
-		provStat = (ProvenanceStmt *) analyzeParseModel((Node *) provStat);
-		DEBUG_NODE_BEATIFY_LOG("analysis:\n", provStat);
-		qoModel = translateParseOracle((Node *) provStat);
-		DEBUG_NODE_BEATIFY_LOG("qo model:\n", provStat);
-		qoModel = provRewriteQBModel(qoModel);
-		DEBUG_NODE_BEATIFY_LOG("after prov rewrite:\n", provStat);
-		sql = serializeOperatorModel(qoModel);
-		ERROR_LOG("SERIALIZED SQL Reenactment For Original update:\n%s", sql);
-
+	        //replace new update with original updates and create reenactment again
+	        popHeadOfListP(updates);
+	        updates = appendToHeadOfList(updates, originalUp);
+	        provStat = createProvenanceStmt((Node *) updates);
+	        provStat->provType = PROV_NONE;
+	        provStat->inputType = PROV_INPUT_REENACT;
+	        DEBUG_NODE_BEATIFY_LOG("prov:\n", provStat);
+	        provStat = (ProvenanceStmt *) analyzeParseModel((Node *) provStat);
+	        DEBUG_NODE_BEATIFY_LOG("analysis:\n", provStat);
+	        qoModel = translateParseOracle((Node *) provStat);
+	        DEBUG_NODE_BEATIFY_LOG("qo model:\n", provStat);
+	        qoModel = provRewriteQBModel(qoModel);
+	        DEBUG_NODE_BEATIFY_LOG("after prov rewrite:\n", provStat);
+	        qoModel = optimizeOperatorModel(qoModel);
+	        DEBUG_NODE_BEATIFY_LOG("after opt:\n", provStat);
+	        sql = serializeOperatorModel(qoModel);
+	        ERROR_LOG("SERIALIZED SQL Reenactment For Original update:\n%s", sql);
+	    }
+	    ON_EXCEPTION
+	    {
+	        // if an exception is thrown then the query memory context has been
+	        // destroyed and we can directly create an empty string in the callers
+	        // context
+	        DEBUG_LOG("allocated in memory context: %s", getCurMemContext()->contextName);
+	    }
+	    END_ON_EXCEPTION
 ////////////////////////////
 		/*
 		 Node *originalUp;
