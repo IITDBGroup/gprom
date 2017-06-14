823f32defcfc67be88b7f4626f919f56782cf0ea
Boris Glavic <lordpretzel@gmail.com>
2017-06-14 10:12:54 -0500

    -updated manual pages - fixed bug with MINUS which was transalted into UNION

--- src/model/query_block/query_block.c
+++ src/model/query_block/query_block.c
@@ -23,12 +23,14 @@ createSetQuery(char *setOp, boolean all, Node *lChild,
 {
     SetQuery *result = makeNode(SetQuery);
 
-    if (!strcmp(setOp, "UNION"))
+    if (streq(setOp, "UNION"))
         result->setOp = SETOP_UNION;
-    if (!strcmp(setOp, "INTERSECT"))
+    else if (streq(setOp, "INTERSECT"))
         result->setOp = SETOP_INTERSECTION;
-    if (!strcmp(setOp, "MINUS"))
+    else if (streq(setOp, "MINUS"))
         result->setOp = SETOP_DIFFERENCE;
+    else
+        FATAL_LOG("set operation has to be one of UNION, INTERSECT, MINUS and not <%s>", setOp);
 
     result->all = all;
     result->selectClause = NIL;
