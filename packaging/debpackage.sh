7951292b0349c2126ddc74a7231039724900c6d8
Boris Glavic <lordpretzel@gmail.com>
2017-12-12 09:06:02 -0600

    fixed

--- packaging/debpackage.sh
+++ packaging/debpackage.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 APP_NAME=gprom
-VERSION=0.0.1
-TMP_DIR=../dpkg
-PACKAGE_FILES=./debfiles
-TAR_PACKAGE=${APP_NAME}-${VERSION}.tar.gz
+VERSION=`../configure --version | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+'`
+
+pushd $(dirname "${0}") > /dev/null
+BASEDIR=$(pwd -L)
+popd > /dev/null
+
+TMP_DIR=${BASEDIR}/../dpkg
+PACKAGE_FILES=${BASEDIR}/debfiles
+TAR_NAME=${APP_NAME}-${VERSION}.tar.gz
+TAR_PACKAGE=${BASEDIR}/../${TAR_NAME}
 
 # Create a deb package
 rm -rf ${TMP_DIR}
 mkdir -p ${TMP_DIR}
-cp ../${TAR_PACKAGE} ${TMP_DIR}/
+cp ${TAR_PACKAGE} ${TMP_DIR}/
 echo "---------- UNTAR"
-tar --directory ${TMP_DIR}/ -xzf ${TMP_DIR}/${TAR_PACKAGE}
+tar --directory ${TMP_DIR}/ -xzf ${TMP_DIR}/${TAR_NAME}
 
 echo "---------- PREPARE"
 pushd ${TMP_DIR}/${APP_NAME}-${VERSION}/
-dh_make --single --copyright gpl -e bglavic@iit.edu -f ../${TAR_PACKAGE} -y
+dh_make --single --copyright gpl -e bglavic@iit.edu -f ../${TAR_NAME} -y
 popd
 cp ${PACKAGE_FILES}/changelog ${PACKAGE_FILES}/control ${PACKAGE_FILES}/copyright ${PACKAGE_FILES}/rules ${TMP_DIR}/${APP_NAME}-${VERSION}/debian/
 
