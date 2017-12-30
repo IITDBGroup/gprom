e3f32c824dcb88c3c58e60f654d8d27774277241
Boris Glavic <lordpretzel@gmail.com>
2017-12-30 12:10:49 -0600

    updated travis build

--- travis/build.sh
+++ travis/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# function to test exist status
+testexit()
+{
+	EXIT_STATUS=$?
+	if [ "x${EXIT_STATUS}" != "x0" ]; then
+		echo "COMMAND EXITED WITH STATUS ${EXIT_STATUS}"
+		exit ${EXIT_STATUS}
+	fi
+}
+
 # move to parent directory for build
 pushd $(dirname "${0}") > /dev/null
 DIR=$(pwd -L)
@@ -7,9 +17,16 @@ popd > /dev/null
 pushd ${DIR}/..
 
 docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make -f Makefile.maintainer
-docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ./configure --with-oci-headers=/opt/oracle/instantclient/sdk/include/ --with-oci-lib=/opt/oracle/instantclient
+testexit
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ./configure --with-oci-headers=/usr/local/oracle/include/oracle/12.2/client64/ --with-oci-lib=/usr/local/oracle/lib/oracle/12.2/client64/lib/
+testexit
 docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make
+testexit
 docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make check
+testexit
 docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make dist
+testexit
 
 popd > /dev/null
+
+exit 0
