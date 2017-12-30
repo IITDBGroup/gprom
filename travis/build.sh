dcada9e8aff67bfed605b7c7c57b07996181b8e2
Boris Glavic <lordpretzel@gmail.com>
2017-12-30 14:33:39 -0600

    updated crosscompilation task in ant to be able to operate when called within a docker container

--- travis/build.sh
+++ travis/build.sh
@@ -16,15 +16,36 @@ DIR=$(pwd -L)
 popd > /dev/null
 pushd ${DIR}/..
 
+echo "************************* RUN AUTOTOOLS SETUP"
 docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make -f Makefile.maintainer
 testexit
+
+echo "************************* RUN CONFIGURE"
 docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ./configure --with-oci-headers=/usr/local/oracle/include/oracle/12.2/client64/ --with-oci-lib=/usr/local/oracle/lib/oracle/12.2/client64/lib/
 testexit
+
+echo "************************* RUN MAKE"
 docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make
 testexit
+
+echo "************************* RUN MAKE CHECK"
 docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make check
 testexit
+
+echo "************************* RUN MAKE DIST"
 docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make dist
+testexit
+
+echo "************************* RUN MAKE DISTCHECK"
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make distcheck
+testexit
+
+echo "************************* RUN LIBGPROM CROSS-COMPILATION"
+docker run --rm --name dockbuild -v /var/run/docker.sock:/var/run/docker.sock -e GPROM_HOST_DIR="$(pwd)" -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ant jar-fat
+testexit
+
+echo "************************* RUN REBUILD JAR WITH ALL NATIVE LIBRARIES"
+docker run --rm --name dockbuild -v /var/run/docker.sock:/var/run/docker.sock -e GPROM_HOST_DIR="$(pwd)" -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ant jar
 testexit
 
 popd > /dev/null
