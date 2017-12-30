4d13ad3d7d7e2a7d590f91cfa792300dd30201bd
Boris Glavic <lordpretzel@gmail.com>
2017-12-29 22:41:57 -0600

    added travis docker build script

---
+++ travis/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# move to parent directory for build
+pushd $(dirname "${0}") > /dev/null
+DIR=$(pwd -L)
+popd > /dev/null
+pushd ${DIR}/..
+
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make -f Makefile.maintainer
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ./configure --with-oci-headers=/opt/oracle/instantclient/sdk/include/ --with-oci-lib=/opt/oracle/instantclient
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make
+# what to do about java blackboxtests? docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make check
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make dist
+
+popd > /dev/null
