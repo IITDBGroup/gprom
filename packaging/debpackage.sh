093766cc4459f3bc023abdd630a3f2fef8b4cf6d
Boris Glavic <lordpretzel@gmail.com>
2018-01-04 22:11:51 -0600

    debpackage build checks for exit status of steps

--- packaging/debpackage.sh
+++ packaging/debpackage.sh
@@ -6,7 +6,7 @@ testexit()
 	LOG_FILE="$1"
 	EXIT_STATUS=$?
 	if [ "x${EXIT_STATUS}" != "x0" ]; then
-		echo "COMMAND EXITED WITH STATUS ${EXIT_STATUS}"
+	        echo "COMMAND EXITED WITH STATUS ${EXIT_STATUS}"
 		echo "LOG OUTPUT:"
 		cat ${LOG_FILE}
 		exit ${EXIT_STATUS}
@@ -33,29 +33,29 @@ rm -rf ${TMP_DIR}
 mkdir -p ${TMP_DIR}
 cp ${TAR_PACKAGE} ${TMP_DIR}/
 echo "---------- UNTAR"
-tar --directory ${TMP_DIR}/ -xzf ${TMP_DIR}/${TAR_NAME}
-testexit
+tar --directory ${TMP_DIR}/ -xzf ${TMP_DIR}/${TAR_NAME} > log.debpackage >&1
+testexit log.debpackage
 
 echo "---------- PREPARE"
 pushd ${TMP_DIR}/${APP_NAME}-${VERSION}/
 echo "dh_make --single --copyright gpl -e bglavic@iit.edu -f ${BASEDIR}/../${TAR_NAME} -y"
-dh_make --single --copyright gpl -e bglavic@iit.edu -f ${BASEDIR}/../${TAR_NAME} -y
-testexit
+dh_make --single --copyright gpl -e bglavic@iit.edu -f ${BASEDIR}/../${TAR_NAME} -y > log.debpackage >&1
+testexit log.debpackage
 popd
-cp ${PACKAGE_FILES}/changelog ${PACKAGE_FILES}/control ${PACKAGE_FILES}/copyright ${PACKAGE_FILES}/rules ${TMP_DIR}/${APP_NAME}-${VERSION}/debian/
-testexit
+cp ${PACKAGE_FILES}/changelog ${PACKAGE_FILES}/control ${PACKAGE_FILES}/copyright ${PACKAGE_FILES}/rules ${TMP_DIR}/${APP_NAME}-${VERSION}/debian/ > log.debpackage >&1
+testexit log.debpackage
 
 #exit 0
 
 echo "---------- BUILD PACKAGE"
 pushd ${TMP_DIR}/${APP_NAME}-${VERSION}/
 pwd
-rm ./debian/README.Debian debian/*.ex debian/*.EX
-testexit
-dpkg-buildpackage -rfakeroot
-testexit
+rm ./debian/README.Debian debian/*.ex debian/*.EX > log.debpackage >&1
+testexit log.debpackage
+dpkg-buildpackage -rfakeroot > log.debpackage >&1
+testexit log.debpackage
 popd
 
-mv ${TMP_DIR}/*.deb ${BASEDIR}/
+cp ${TMP_DIR}/*.deb ${BASEDIR}/
 testexit
 rm -rf ${TMP_DIR}
