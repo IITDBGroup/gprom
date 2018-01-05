d068335a286dcaa9331687c7e1abee0b62304895
Boris Glavic <lordpretzel@gmail.com>
2018-01-05 12:07:01 -0600

    now passing correct library version to libtool

--- travis/build.sh
+++ travis/build.sh
@@ -20,40 +20,40 @@ popd > /dev/null
 pushd ${DIR}/..
 
 echo "************************* RUN AUTOTOOLS SETUP"
-docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make -f Makefile.maintainer > log.autotools 2>&1
-testexit log.autotools
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make -f Makefile.maintainer > log_autotools.log 2>&1
+testexit log_autotools.log
 
 echo "************************* RUN CONFIGURE"
-docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ./configure --with-oci-headers=/usr/local/oracle/include/oracle/12.2/client64/ --with-oci-lib=/usr/local/oracle/lib/oracle/12.2/client64/lib/ > log.configure 2>&1
-testexit log.configure
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ./configure --with-oci-headers=/usr/local/oracle/include/oracle/12.2/client64/ --with-oci-lib=/usr/local/oracle/lib/oracle/12.2/client64/lib/ > log_configure.log 2>&1
+testexit log_configure.log
 
 echo "************************* RUN MAKE"
-docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make > log.make 2>&1
-testexit log.make
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make -j32 > log_make.log 2>&1
+testexit log_make.log
 
 echo "************************* RUN MAKE CHECK"
-docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make check > log.check 2>&1
-testexit log.check
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make -j32 check > log_check.log 2>&1
+testexit log_check.log
 
 echo "************************* RUN MAKE DIST"
-docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make dist > log.dist 2>&1
-testexit log.dist
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make -j32 dist > log_dist.log 2>&1
+testexit log_dist.log
 
 echo "************************* RUN MAKE DISTCHECK"
-docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make distcheck > log.distcheck 2>&1
-testexit log.distcheck
+docker run --rm --name dockbuild -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make -j32 distcheck > log_distcheck.log 2>&1
+testexit log_distcheck.log
 
 echo "************************* RUN MAKE PKG-DEB"
-docker run --rm --name dockbuild -e USER=root -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make pkg-deb > log.deb 2>&1
-testexit log.deb
+docker run --rm --name dockbuild -e USER=root -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest make pkg-deb > log_deb.log 2>&1
+testexit log_deb.log
 
 echo "************************* RUN LIBGPROM CROSS-COMPILATION"
-docker run --rm --name dockbuild -v /var/run/docker.sock:/var/run/docker.sock -e GPROM_HOST_DIR="$(pwd)" -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ant -Dskipivy=true jar-fat > log.jar-fat 2>&1
-testexit log.jar-fat
+docker run --rm --name dockbuild -v /var/run/docker.sock:/var/run/docker.sock -e GPROM_HOST_DIR="$(pwd)" -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ant -Dskipivy=true jar-fat > log_jar-fat.log 2>&1
+testexit log_jar-fat.log
 
 echo "************************* RUN REBUILD JAR WITH ALL NATIVE LIBRARIES"
-docker run --rm --name dockbuild -v /var/run/docker.sock:/var/run/docker.sock -e GPROM_HOST_DIR="$(pwd)" -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ant -Dskipivy=true jar > log.jar 2>&1
-testexit log.jar
+docker run --rm --name dockbuild -v /var/run/docker.sock:/var/run/docker.sock -e GPROM_HOST_DIR="$(pwd)" -v "$(pwd)":/gprom iitdbgroup/gprom_travis:latest ant -Dskipivy=true jar > log_jar.log 2>&1
+testexit log_jar.log
 
 popd > /dev/null
 
