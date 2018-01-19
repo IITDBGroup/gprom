03bf24a15a23e2e335b1ae89c0a5f60af5d05440
Boris Glavic <lordpretzel@gmail.com>
2018-01-19 11:04:59 -0600

    added blankdb

--- clients/pythonweb/provgraph_web/connsettings.py
+++ clients/pythonweb/provgraph_web/connsettings.py
@@ -1,7 +1,13 @@
 # Please change host, user, and password
-GPROM_HOST=None
+GPROM_HOST='127.0.0.1'
 GPROM_DB='orcl'
-GPROM_USER=None
-GPROM_PASSWD=None
+GPROM_USER='fga_user'
+GPROM_PASSWD='fga'
 GPROM_PORT=1521
 GPROM_BACKEND='oracle'
+# GPROM_HOST=None
+# GPROM_DB='orcl'
+# GPROM_USER=None
+# GPROM_PASSWD=None
+# GPROM_PORT=1521
+# GPROM_BACKEND='oracle'
