63fda2e4a58f46699b3f3af93bf2da76e68f7c02
Boris Glavic <lordpretzel@gmail.com>
2018-01-19 11:06:27 -0600

    standard connection settings

--- clients/pythonweb/provgraph_web/connsettings.py
+++ clients/pythonweb/provgraph_web/connsettings.py
@@ -1,13 +1,7 @@
 # Please change host, user, and password
-GPROM_HOST='127.0.0.1'
+GPROM_HOST=None
 GPROM_DB='orcl'
-GPROM_USER='fga_user'
-GPROM_PASSWD='fga'
+GPROM_USER=None
+GPROM_PASSWD=None
 GPROM_PORT=1521
 GPROM_BACKEND='oracle'
-# GPROM_HOST=None
-# GPROM_DB='orcl'
-# GPROM_USER=None
-# GPROM_PASSWD=None
-# GPROM_PORT=1521
-# GPROM_BACKEND='oracle'
