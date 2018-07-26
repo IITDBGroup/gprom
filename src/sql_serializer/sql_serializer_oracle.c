d6dcc4210b26e607d3fff857c2ecbc76a290e548
Xing Niu <xniu7@hawk.iit.edu>
2018-07-26 04:33:02 -0500

    fix set op

--- src/sql_serializer/sql_serializer_oracle.c
+++ src/sql_serializer/sql_serializer_oracle.c
@@ -1806,7 +1806,7 @@ serializeProjectionAndAggregation (QueryBlockMatch *m, StringInfo select,
         FOREACH(List, attrs, fac->fromAttrs)
         {
             FOREACH(char,name,attrs)
-                 inAttrs = appendToTailOfList(inAttrs, CONCAT_STRINGS("F", gprom_itoa(fromItem), ".", name));
+                 inAttrs = appendToTailOfList(inAttrs, CONCAT_STRINGS("F", gprom_itoa(fromItem), "_0.", name));
             fromItem++;
         }
 
