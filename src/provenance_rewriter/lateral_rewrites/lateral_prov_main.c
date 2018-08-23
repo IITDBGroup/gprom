89214b5461ce56182deba1111e65655e994768ec
Xing Niu <xniu7@hawk.iit.edu>
2018-08-22 21:05:16 -0500

    adapt nesting operator (condition and schema: bool -> int/long) in lateral_rewrite

--- src/provenance_rewriter/lateral_rewrites/lateral_prov_main.c
+++ src/provenance_rewriter/lateral_rewrites/lateral_prov_main.c
@@ -6,6 +6,7 @@
  */
 
 #include "common.h"
+#include "mem_manager/mem_mgr.h"
 #include "configuration/option.h"
 #include "provenance_rewriter/lateral_rewrites/lateral_prov_main.h"
 #include "provenance_rewriter/prov_utility.h"
@@ -14,18 +15,15 @@
 #include "model/query_operator/operator_property.h"
 #include "log/logger.h"
 #include "model/list/list.h"
-
-#define AGGNAME_SUM backendifyIdentifier("sum")
-#define AGGNAME_COUNT backendifyIdentifier("count")
-#define AGGNAME_AVG backendifyIdentifier("avg")
-#define AGGNAME_MIN backendifyIdentifier("min")
-#define AGGNAME_MAX backendifyIdentifier("max")
+#include "utility/string_utils.h"
 
 static List *lateralRewriteQueryList(List *list);
 static QueryOperator *lateralRewriteQuery(QueryOperator *input);
 static List *getListNestingOperator (QueryOperator *op);
 static void appendNestingOperator (QueryOperator *op, List **result);
 static int checkAttr (char *name, QueryOperator *op);
+static void adatpUpNestingAttrDataType(QueryOperator *op, DataType nestingAttrDataType, int pos);
+static void getNestCondOperator(Operator *o, List **nestOpLists);
 
 Node *
 lateralTranslateQBModel (Node *qbModel)
@@ -68,6 +66,7 @@ lateralRewriteQuery(QueryOperator *input)
 
 		NestingOperator *no = (NestingOperator *) op;
 		char *aggname;
+		DataType nestingAttrDataType = DT_INT;
 
 		if(no->nestingType == NESTQ_EXISTS)
 		{
@@ -90,11 +89,15 @@ lateralRewriteQuery(QueryOperator *input)
 			Constant *el =  copyObject(c0);
 			CaseExpr *caseExpr = createCaseExpr(NULL, singleton(when), (Node *) el);
 
-			ProjectionOperator *proj = createProjectionOp(singleton(caseExpr), (QueryOperator *) agg, NIL, singleton("nesting_eval_1"));
+			char *attrName = getTailOfListP(getAttrNames(op->schema));
+			ProjectionOperator *proj = createProjectionOp(singleton(caseExpr), (QueryOperator *) agg, NIL, singleton(strdup(attrName)));
 
 			((QueryOperator *) agg)->parents = singleton(proj);
 			((QueryOperator *) proj)->parents = singleton(op);
 			op->inputs = LIST_MAKE(lChild, proj);
+
+			//used to change nesting_eval_1 datatype from boolean in nesting op
+			nestingAttrDataType = (DataType) getTailOfListInt(getDataTypes(((QueryOperator *) proj)->schema));
 		}
 		else if (no->nestingType == NESTQ_ANY || no->nestingType == NESTQ_ALL)
 		{
@@ -141,11 +144,16 @@ lateralRewriteQuery(QueryOperator *input)
 			FunctionCall *aggFunc = createFunctionCall(aggname,
 					singleton(copyObject(projAttrRef)));
 			List *aggrs = singleton(aggFunc);
-			AggregationOperator *agg = createAggregationOp(aggrs, NIL, (QueryOperator *) proj, NIL, singleton("nesting_eval_1"));
+			char *attrName = getTailOfListP(getAttrNames(op->schema));
+			AggregationOperator *agg = createAggregationOp(aggrs, NIL, (QueryOperator *) proj, NIL, singleton(strdup(attrName)));
 
 			((QueryOperator *) agg)->parents = singleton(op);
 			((QueryOperator *) proj)->parents = singleton(agg);
 			op->inputs = LIST_MAKE(lChild, agg);
+
+			//used to change nesting_eval_1 datatype from boolean in nesting op
+			//nestingAttrDataType = typeOf(aggFunc);
+			nestingAttrDataType = (DataType) getTailOfListInt(getDataTypes(((QueryOperator *) agg)->schema));
 		}
 		else if(no->nestingType == NESTQ_UNIQUE)
 		{
@@ -159,9 +167,96 @@ lateralRewriteQuery(QueryOperator *input)
 		}
 
 		no->nestingType = NESTQ_LATERAL;
+
+		////change nesting_eval_1, nesting_eval_2, nesting_eval_... datatype from boolean in nesting op
+		AttributeDef *nestAttrDef = (AttributeDef *) getTailOfListP(op->schema->attrDefs);
+		nestAttrDef->dataType = nestingAttrDataType;
+		int pos = LIST_LENGTH(op->schema->attrDefs) - 1;
+		adatpUpNestingAttrDataType(op, nestingAttrDataType, pos);
+	}
+
+	//adapt selection: one in condition(nesting_eval_1 = True -> nesting_eval_1 = 1)
+	//				 : one in schema (loop nestOpList again since the above loop adapt all nest op firstly, selection need to use the schema nest op)
+	FOREACH(QueryOperator, op, nestOpList)
+	{
+		   QueryOperator *selOp = (QueryOperator *) getHeadOfListP(op->parents);
+		   if(isA(selOp, SelectionOperator))
+		   {
+			   SelectionOperator *sel = (SelectionOperator *) selOp;
+			   List *nestOpList = NIL;
+			   getNestCondOperator((Operator *) sel->cond, &nestOpList);
+
+			   //one
+			   //adapt cond
+			   FOREACH(Operator, o, nestOpList)
+			   {
+				AttributeReference *a = getNthOfListP(o->args, 0);
+				//adapt nesting_eval_1 datatype from its children
+				AttributeDef *ad = getAttrDefByName(OP_LCHILD(selOp), a->name);
+				a->attrType = ad->dataType;
+
+				//change constant datatype from bool to int
+				o->args = removeFromTail(o->args);
+				o->args = appendToTailOfList(o->args, createConstInt(1));
+			   }
+
+			   //one
+			   //adapt schema
+			   FOREACH(AttributeDef, ad, selOp->schema->attrDefs)
+			   {
+				   if(strlen(ad->attrName) >= 14)
+				   {
+					   char *prefix = substr(ad->attrName, 0, 12);
+					   if(streq(prefix, "nesting_eval_"))
+					   {
+						   AttributeDef *adChild = getAttrDefByName(OP_LCHILD(selOp), ad->attrName);
+						   ad->dataType = adChild->dataType;
+					   }
+				   }
+		   	   }
+		}
+
 	}
 
 	return input;
+}
+
+static void
+adatpUpNestingAttrDataType(QueryOperator *op, DataType nestingAttrDataType, int pos)
+{
+	FOREACH(QueryOperator, o, op->parents)
+	{
+		if(isA(o, NestingOperator))
+		{
+			AttributeDef *ad = getNthOfListP(o->schema->attrDefs, pos);
+			ad->dataType = nestingAttrDataType;
+			adatpUpNestingAttrDataType(o, nestingAttrDataType, pos);
+		}
+	}
+}
+
+static void
+getNestCondOperator(Operator *o, List **nestOpLists)
+{
+	if(streq(o->name, "="))
+	{
+		if(isA(getNthOfListP(o->args, 0), AttributeReference) && isA(getNthOfListP(o->args, 1), Constant))
+		{
+			AttributeReference *a = getNthOfListP(o->args, 0);
+			Constant *c = getNthOfListP(o->args, 1);
+			if(strlen(a->name) >= 14)
+			{
+				char *prefix = substr(a->name, 0, 12);
+				if(streq(prefix, "nesting_eval_") && c->constType == 4)
+					*nestOpLists = appendToTailOfList(*nestOpLists, o);
+			}
+		}
+	}
+	else if(streq(o->name, "OR") || streq(o->name, "AND") )
+	{
+		getNestCondOperator((Operator *) getNthOfListP(o->args, 0), nestOpLists);
+		getNestCondOperator((Operator *) getNthOfListP(o->args, 1), nestOpLists);
+	}
 }
 
 
