b1cc9c4cec6423571186a168c7f5c4f6b3ed1263
fengsu91 <sfeng14@hawk.iit.edu>
2019-10-24 20:51:41 -0500

    update

--- include/provenance_rewriter/uncertainty_rewrites/uncert_rewriter.h
+++ include/provenance_rewriter/uncertainty_rewrites/uncert_rewriter.h
@@ -5,8 +5,17 @@
 #define UNCERTAIN_ROW_ATTR backendifyIdentifier("R")
 #define UNCERTAIN_FULL_ROW_ATTR getUncertString(UNCERTAIN_ROW_ATTR)
 #define UNCERTAIN_MAKER_FUNC_NAME "UNCERT"
+#define ROW_CERTAIN "CET_R"
+#define ROW_BESTGUESS "BST_R"
+#define ROW_POSSIBLE "POS_R"
+#define ROW_CERTAIN_TWO "CET_R1"
+#define ROW_BESTGUESS_TWO "BST_R1"
+#define ROW_POSSIBLE_TWO "POS_R1"
 
 extern QueryOperator *rewriteUncert(QueryOperator *op);
+extern QueryOperator *rewriteRange(QueryOperator *op);
 extern Node *getUncertaintyExpr(Node *expr, HashMap *hmp); //hashmap of attrref->attrref(attribute->attribute uncertainty)
 extern Node *removeUncertOpFromExpr(Node *expr);
 extern char *getUncertString(char *in);
+extern char *getUBString(char *in);
+extern char *getLBString(char *in);
