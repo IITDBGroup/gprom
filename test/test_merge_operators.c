595e526824bb63a3ca491e33a7d87c899e42aee2
BELKHEDRA Yamine <Yamine.Belkhedra@bordeaux-inp.fr>
2023-07-04 23:03:17 +0200

    adding a test for combine row by attribute and merging queries

--- test/test_merge_operators.c
+++ test/test_merge_operators.c
@@ -13,42 +13,79 @@
 #include "test_main.h"
 #include "common.h"
 
+#include "rewriter.h"
 #include "parser/parser.h"
+#include "execution/executor.h"
 #include "provenance_rewriter/uncertainty_rewrites/uncert_rewriter.h"
 #include "model/query_operator/query_operator.h"
 #include "analysis_and_translate/analyzer.h"
 #include "analysis_and_translate/translator.h"
 
 
+static rc testCombineRowByAttr(void);
 static rc testMerge1(void);
 
 rc
 testMergeOperators(void)
 {
+    RUN_TEST(testCombineRowByAttr(), "test combine row by attribute");
     RUN_TEST(testMerge1(), "test merge 1");
 
     return PASS;
 }
 
+static rc
+testCombineRowByAttr(void)
+{
+    QueryOperator *op;
+    char *attrName = "a";
+    char *command = "SELECT * FROM uadb1;";
+    char *rewrittenSQL = NULL;
+
+    Node *t = parseFromString(command);
+    op = (QueryOperator *) getHeadOfList((List *) translateParse(t))->data.ptr_value;
+
+    QueryOperator *result = combineRowByAttr(op, attrName, TRUE);
+
+    INFO_OP_LOG("result", result);
+
+    if (result == NULL)
+        return FAIL;
+
+    rewrittenSQL = generatePlan((Node *)result, FALSE);
+
+    printf("rewritten SQL: %s\n", rewrittenSQL);
+
+    execute(rewrittenSQL);
+
+    return PASS;
+}
 
 static rc
 testMerge1(void)
 {
     QueryOperator *op1;
     QueryOperator *op2;
-    char *attrName = "A";
-    char *command = "SELECT * FROM r;";
+    char *attrName = "a";
+    char *command1 = "SELECT * FROM uadb1;";
+    char *command2 = "SELECT * FROM uadb2;";
+    char *rewrittenSQL = NULL;
 
-    Node *t = parseFromString(command);
-    printf("analyzed query: %s\n", beatify(nodeToString(t)));
-    printf("Translation and taking the head of the list\n");
-    op1 =  (QueryOperator *) getHeadOfList((List *) translateParse(t))->data.ptr_value;
-    op2 = (QueryOperator *) getHeadOfList((List *) translateParse(t))->data.ptr_value;
 
-    printf("op type is %s\n", NodeTagToString(op1->type));
+    Node *t1 = parseFromString(command1);
+    Node *t2 = parseFromString(command2);
+    op1 =  (QueryOperator *) getHeadOfList((List *) translateParse(t1))->data.ptr_value;
+    op2 = (QueryOperator *) getHeadOfList((List *) translateParse(t2))->data.ptr_value;
 
-    printf("translated query: %s\n", beatify(nodeToString(op1)));
+    QueryOperator *result = mergeQueries(op1, op2, attrName);
+    if (result == NULL)
+        return FAIL;
 
-    mergeQueries(op1, op2, attrName);
+    rewrittenSQL = generatePlan((Node *)result, FALSE);
+
+    printf ("rewritten SQL: %s\n", rewrittenSQL);
+
+    execute(rewrittenSQL);
+
     return PASS;
 }
