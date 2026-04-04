1a9f0ec5e0ddee5c8fc7d19ad491b2663ec15445
Boris Glavic <lordpretzel@gmail.com>
2026-04-03 19:00:06 -0500

    update versioning script and manually fix missing version updates for v1.0.5

--- update_version.sh
+++ update_version.sh
@@ -13,11 +13,11 @@ VERSION_NUMBER="${1}"
 echo "prepare version ${VERSION_NUMBER}"
 
 # update man page
-sed -i "1s/.*/.TH gprom 1 \"${MYDATE}\" \"version ${VERSION_NUMBER}\"/" ${BASEDIR}/doc/gprom.man
+gsed -i "1s/.*/.TH gprom 1 \"${MYDATE}\" \"version ${VERSION_NUMBER}\"/" ${BASEDIR}/doc/gprom.man
 
 # create changelog entry
-sed -i "s/revision=\"[0-9]\+\.[0-9]\+\.[0-9]\+\"/revision=\"${VERSION_NUMBER}\"/g" ivy.xml
-sed -i "s/AC_INIT(\[GProM\],\[[0-9]\+\.[0-9]\+\.[0-9]\+\],\[bglavic@uic.edu\])/AC_INIT([GProM],[${VERSION_NUMBER}],[bglavic@uic.edu])/g" ${BASEDIR}/configure.ac
+gsed -i "s/revision=\"[0-9]\+\.[0-9]\+\.[0-9]\+\"/revision=\"${VERSION_NUMBER}\"/g" ivy.xml
+gsed -i "s/AC_INIT(\[GProM\],\[[0-9]\+\.[0-9]\+\.[0-9]\+\],\[bglavic@uic.edu\])/AC_INIT([GProM],[${VERSION_NUMBER}],[bglavic@uic.edu])/g" ${BASEDIR}/configure.ac
 
 CHANGELOG_FILE=${BASEDIR}/packaging/debfiles/changelog
 CHANGELOG_ENTRY="gprom (${VERSION_NUMBER}-1) unstable; urgency=low
@@ -42,11 +42,21 @@ GIT_TAG="v${VERSION_NUMBER}"
 echo "check whether git version has tag ${GIT_TAG}"
 HAS_TAG=`git show-ref --tags | grep -c "${GIT_TAG}"`
 
+# create tar.gz archive
+make -j dist
+
+# create tag and push it
 if [ "X${HAS_TAG}" == "X0" ]; then
     echo "tag not present, create it"
     git commit -am "commit updating reference to version in files to ${GIT_TAG}"
     git tag -am "GProM version ${GIT_TAG}" ${GIT_TAG}
+    git push origin
+    git push --tags origin
 fi
 
-#sed -i -e 's/VERSION/${VERSION}/g' ${TMP_DIR}/${APP_NAME}-${VERSION}/debian/control
+# create github release
+gh release create v${VERSION_NUMBER} --verify-tag -t "v${VERSION_NUMBER}" --notes-from-tag gprom-${VERSION_NUMBER}.tar.gz
+
+# update debian package control file
+gsed -i -e 's/VERSION/${VERSION}/g' ${TMP_DIR}/${APP_NAME}-${VERSION}/debian/control
 popd > /dev/null
